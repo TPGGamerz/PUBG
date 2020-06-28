ef2524a81b81d7911bdd89ec54b1827b140005df
RAGEGAMERZ <66334354+RAGEGAMERZ@users.noreply.github.com>
2020-06-28 21:10:31 +0530

    Update scorelist.js

--- scorelist.js
+++ scorelist.js
@@ -25,53 +25,53 @@ var arr = [];
 
 arr[0] = new Team("OG             ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[1] = new Team(" XTRANJEROS    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[1] = new Team(" XTRANJEROS    ", 1, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[2] = new Team("TEAM PWOLI     ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[2] = new Team("TEAM PWOLI     ", 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[3] = new Team("TEAM TTx       ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[4] = new Team("BORNKILLERZ    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[4] = new Team("BORNKILLERZ    ", 7, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[5] = new Team("TEAM I͜͡NSANE    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[6] = new Team("SpYDeR         ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[6] = new Team("SpYDeR         ", 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[7] = new Team("TEAM BT        ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[7] = new Team("TEAM BT        ", 10, 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[8] = new Team(" KFG           ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[8] = new Team(" KFG           ", 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[9] = new Team("BRTHxkerala    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[9] = new Team("BRTHxkerala    ", 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[10] = new Team("TEAM 6AM      ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[11] = new Team("TEAM IDEAL-1  ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[11] = new Team("TEAM IDEAL-1  ", 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[12] = new Team("TEAM IDEAL-2  ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[13] = new Team("Team ZOE      ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[13] = new Team("Team ZOE      ", 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[14] = new Team("GREAT LEOS    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[14] = new Team("GREAT LEOS    ",15, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[15] = new Team("TxT           ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[16] = new Team("KɸR〆         ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[16] = new Team("KɸR〆         ", 9, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[17] = new Team("RAPTORS       ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[18] = new Team("white walkers ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[19] = new Team("KL01彡CREW    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[19] = new Team("KL01彡CREW    ", 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[20] = new Team("RQD           ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[21] = new Team("M24 ßoys      ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[21] = new Team("M24 ßoys      ", 4, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[22] = new Team("TAFrecruit    ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[22] = new Team("TAFrecruit    ", 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[23] = new Team("8DPKerala     ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[24] = new Team("TPG๛          ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[24] = new Team("TPG๛          ", 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr.length-1 ; j++) {
