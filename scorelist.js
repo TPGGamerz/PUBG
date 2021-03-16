cc495feb6ce893db6a5cb98f871d783de7a64f0c
TPGGamerz <66334354+TPGGamerz@users.noreply.github.com>
2021-03-16 23:32:42 +0530

    Update scorelist.js

--- scorelist.js
+++ scorelist.js
@@ -22,43 +22,43 @@ var arr = [];
 //arr[x] = new Team("Team name", kp1 , pp1 , kp2 , pp2 , kp3 , pp3 , kp4 , pp4 , kp5 , pp5, kp6, pp6, kp7, pp7);
 
 
-arr[0] = new Team(" 7xTequilla          ", 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[0] = new Team(" 7xTequilla          ", 4, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[1] = new Team(" XXX                 ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[2] = new Team(" CloudX              ", 18, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[2] = new Team(" CloudX              ", 18, 10, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[3] = new Team(" DOA Gaming          ", 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[4] = new Team(" TEAM TARGET         ", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[4] = new Team(" TEAM TARGET         ", 0, 1, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[5] = new Team(" LANNISTER Esports   ", 8, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[5] = new Team(" LANNISTER Esports   ", 8, 3, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[6] = new Team("  TAKZ               ", 18, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[6] = new Team("  TAKZ               ", 18, 8, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[7] = new Team(" MALLU COMRADE       ", 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[7] = new Team(" MALLU COMRADE       ", 4, 4, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[8] = new Team(" FERRICZ             ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[8] = new Team(" FERRICZ             ", 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[9] = new Team("  TPG                ", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[9] = new Team("  TPG                ", 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[10] = new Team(" TEXAS 14Esports    ", 8, 1, 26, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[11] = new Team(" INFACT Esports     ", 8, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[11] = new Team(" INFACT Esports     ", 8, 7, 4, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[12] = new Team(" Kinght Of Mutant   ", 18, 6, 30, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[13] = new Team(" Team Galaxy        ", 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[14] = new Team(" TORPEDO Elites     ", 26, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[14] = new Team(" TORPEDO Elites     ", 26, 20, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[15] = new Team(" ReaperXesports     ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[15] = new Team(" ReaperXesports     ", 0, 0, 26, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[16] = new Team(" XtremeAlpha        ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[16] = new Team(" XtremeAlpha        ", 0, 0, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[17] = new Team(" Brigadianz Esports ", 8, 5, 8, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[18] = new Team(" Gain Official      ", 12, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[18] = new Team(" Gain Official      ", 12, 14, 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[19] = new Team(" 7DE                ", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
