f87bf3e0b4a819dc6bf37e255d1d04628a68bbcf
TPGGamerz <66334354+TPGGamerz@users.noreply.github.com>
2022-02-23 10:42:47 +0530

    Update scorelist.js

--- scorelist.js
+++ scorelist.js
@@ -101,7 +101,7 @@ arr[37] = new Team("LYN  ", 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[38] = new Team("SQ NEMISIS  ", 6, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-arr[39] = new Team("TRz TRIABLZ - Team XR ", 5, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+arr[39] = new Team("TRz TRIABLZ - Team XR ", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 arr[40] = new Team("BYOB  ", 9, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
